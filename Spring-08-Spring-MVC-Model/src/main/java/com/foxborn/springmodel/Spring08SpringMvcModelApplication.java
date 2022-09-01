b9355c9a68d4203f25fa9e91595063153aacf737
bourne23 <ofoxorn@gmail.com>
2022-09-01 13:37:24 -0400

    spring model

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/foxborn/springmodel/Spring08SpringMvcModelApplication.java
@@ -0,0 +1,13 @@
+package com.foxborn.springmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08SpringMvcModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring08SpringMvcModelApplication.class, args);
+    }
+
+}
---
+++ Spring-08-Spring-MVC-Model/src/test/java/com/foxborn/spring08springmvcmodel/Spring08SpringMvcModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foxborn.spring08springmvcmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring08SpringMvcModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
