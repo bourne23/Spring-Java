bc8f317faac6963ebc30245b9e03fec57b1fcc80
bourne23 <ofoxorn@gmail.com>
2022-09-01 11:44:47 -0400

    spring mvc

---
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/foxborn/Spring07SpringMvcControllerApplication.java
@@ -0,0 +1,13 @@
+package com.foxborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07SpringMvcControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring07SpringMvcControllerApplication.class, args);
+    }
+
+}
---
+++ Spring-07-Spring-MVC-Controller/src/test/java/com/foxborn/Spring07SpringMvcControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foxborn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring07SpringMvcControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
