4785e135d57d8bba2ca7e69137f418866a906316
bourne23 <ofoxborn@gmail.com>
2022-10-09 12:52:01 -0400

    Database, JDBC, Spring Data
    Signed-off-by: bourne23 <ofoxorn@gmail.com>

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/foxborn/Spring10OrmTableCreationApplication.java
@@ -0,0 +1,13 @@
+package com.foxborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring10OrmTableCreationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring10OrmTableCreationApplication.class, args);
+    }
+
+}
